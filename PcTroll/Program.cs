48ad57dcb5d0e58f064c87ab5114494b95346a62
xFloki <alexdietta@hotmail.com>
2016-08-15 06:30:38 +0200

    Empezado el hilo de las letras random

--- PcTroll/Program.cs
+++ PcTroll/Program.cs
@@ -2,14 +2,62 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+
+using System.Windows.Forms;
+
+
+
+
+
+//
+//  Nombre Aplicacion: PcTroll
+//  Descripción: Programa que se ejectuca en segundo plano y que genera movimientos aleatorios del rantón, sonidos de sistema, inputs de teclas aleatorios y 
+//  genera popups de error y alerta de modo que el usuario no sabe lo que ocurre con su ordenador 
+//
+
 
 namespace PcTroll
 {
+
     class Program
     {
+        static Random nRandom = new Random();
+        static int numero = 5;
         static void Main(string[] args)
         {
+            //Se generan una serie de hilos para que se ejectuen de manera paralela
+            Thread generaLetrasThread = new Thread(new ThreadStart(generaLetras));
+
+            generaLetrasThread.Start();
+
+
+            Console.ReadLine();
+            generaLetrasThread.Abort();
+        }
+
+        static void generaLetras()
+        {
+            while (true)
+            {
+                //le añadimos una probabilidad de que escriba una letra
+                if (nRandom.Next(10) > 1)
+                {
+                    char letra = (char)((nRandom.Next(26) + 65));
+                    //en caso de que la escriba esa tiene una probabilidad de ser minuscula
+                    if (nRandom.Next(10) > 4)
+                    {
+                        letra = char.ToLower(letra);
+                }
+
+                    SendKeys.SendWait(letra.ToString());
+
+                }
+
+                Thread.Sleep(300);
+            }
+
         }
     }
 }
