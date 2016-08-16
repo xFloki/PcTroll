83453c81c7d83d24be411533e273c0c4e36168c9
xFloki <alexdietta@hotmail.com>
2016-08-17 00:23:23 +0200

    Terminados los 4 threads con sonido, movimientos del raton, inputs de teclas y popups

--- PcTroll/Program.cs
+++ PcTroll/Program.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
+using System.Media;
+
 using System.Windows.Forms;
 
 
@@ -29,12 +31,26 @@ namespace PcTroll
         {
             //Se generan una serie de hilos para que se ejectuen de manera paralela
             Thread generaLetrasThread = new Thread(new ThreadStart(generaLetras));
+            Thread generaMovimientosThread = new Thread(new ThreadStart(generaMovimiento));
+            Thread generaSonidosThread = new Thread(new ThreadStart(generaSonidos));
+            Thread generaPopupsThread = new Thread(new ThreadStart(generaPopups));
+
+            DateTime inicio = DateTime.Now.AddSeconds(180);
+            while (inicio > DateTime.Now) { Thread.Sleep(1000); }
 
             generaLetrasThread.Start();
+            generaMovimientosThread.Start();
+            generaSonidosThread.Start();
+            generaPopupsThread.Start();
 
+            inicio = DateTime.Now.AddSeconds(180);
+            while (inicio > DateTime.Now) { Thread.Sleep(1000); }
 
-            Console.ReadLine();
+            //Console.Read();
             generaLetrasThread.Abort();
+            generaMovimientosThread.Abort();
+            generaSonidosThread.Abort();
+            generaPopupsThread.Abort();
         }
 
         static void generaLetras()
@@ -42,14 +58,14 @@ namespace PcTroll
             while (true)
             {
                 //le añadimos una probabilidad de que escriba una letra
-                if (nRandom.Next(10) > 1)
+                if (nRandom.Next(10) > 7)
                 {
                     char letra = (char)((nRandom.Next(26) + 65));
                     //en caso de que la escriba esa tiene una probabilidad de ser minuscula
                     if (nRandom.Next(10) > 4)
                     {
                         letra = char.ToLower(letra);
-                }
+                    }
 
                     SendKeys.SendWait(letra.ToString());
 
@@ -59,5 +75,100 @@ namespace PcTroll
             }
 
         }
+
+        static void generaMovimiento()
+        {
+            int movimientoX;
+            int movimientoY;
+
+            while (true)
+            {
+                //le añadimos una probabilidad de que escriba una letra
+                if (nRandom.Next(10) > 3)
+                {
+                    movimientoX = nRandom.Next(10) - 5;
+                    movimientoY = nRandom.Next(10) - 5;
+
+                    Cursor.Position = new System.Drawing.Point(Cursor.Position.X + movimientoX, Cursor.Position.Y + movimientoY);
+
+
+                }
+
+                Thread.Sleep(200);
+            }
+
+        }
+
+        static void generaSonidos()
+        {
+            while (true)
+            {
+                //le añadimos una probabilidad de que escriba una letra
+                if (nRandom.Next(20) > 15)
+                {
+                    switch (nRandom.Next(5))
+                    {
+                        case 0:
+                            SystemSounds.Asterisk.Play();
+                            break;
+                        case 1:
+                            SystemSounds.Beep.Play();
+                            break;
+                        case 2:
+                            SystemSounds.Exclamation.Play();
+                            break;
+                        case 3:
+                            SystemSounds.Hand.Play();
+                            break;
+                        case 4:
+                            SystemSounds.Question.Play();
+                            break;
+
+                    }
+                }
+
+                Thread.Sleep(1000);
+            }
+
+        }
+
+        static void generaPopups()
+        {
+            while (true)
+            {
+                //le añadimos una probabilidad de que escriba una letra
+                if (nRandom.Next(100) > 90)
+                {
+
+                    switch (nRandom.Next(2))
+                    {
+                        case 0:
+                            MessageBox.Show(
+                            "Internet explorer ha dejado de funcionar",
+                            "Internet Explorer",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Error);
+                            break;
+
+                        case 1:
+                            MessageBox.Show(
+                            "Batería baja, conecte el cargador",
+                            "Microsoft Windows",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Warning);
+                            break;
+                    }
+                }
+
+                Thread.Sleep(6000);
+            }
+
+        }
+
+
+
+
+
     }
 }
+
